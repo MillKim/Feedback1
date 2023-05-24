087acecb9c2edc66695c6746fb96ac5b15af670a
Mill_Kim <93661380+MillKim@users.noreply.github.com>
2023-05-24 20:27:51 +0900

    13

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ model = RNNModel(output_d, c) #RNNModel 쓰는경우
 # model = LSTMModel(output_d, c) #LSTMModel 쓰는경우
 # model = ATTModel(output_d, c) #ATTModel 쓰는경우
 
-model.load_state_dict(torch.load("./save/"+model_name+".pt")).cpu()
-
+model.load_state_dict(torch.load("./save/"+model_name+".pt"))
 #자신에게 맞는 모델로 부르기
 tokenizer = AutoTokenizer.from_pretrained("./save/"+ model_name) #sp tokenizer 쓰는 경우
 # tokenizer = BertTokenizer.from_pretrained("./save/"+model_name+"-vocab.txt") #bw tokenizer 쓰는경우
